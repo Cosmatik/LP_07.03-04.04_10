4016c3af8e73b0dc85b4f2d2a394cf281b613038
Cosmatik <biznespro2021@yandex.ru>
2023-03-14 13:19:05 +0300

    Добавили изменения к задаче 0

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -7,6 +7,10 @@
 // -3 -> 49
 // -7 -> 49
 
+
+// Console.Write("Введите любое число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+
 // string input = Console.ReadLine();
 // int number = Convert.ToInt32(input);
 Console.Write("Введите целое число: ");
