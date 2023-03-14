82701eb9f531653234511170d7dc82971f07a9e3
Cosmatik <biznespro2021@yandex.ru>
2023-03-14 13:19:55 +0300

    Добавили решение к задаче 13

---
+++ Task13/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 13: 
+// Напишите программу, которая 
+// 1.выводит третью цифру заданного числа или 
+// 2.сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+
+
+int number1 = new Random().Next();
+Console.WriteLine("Компьютер загадывает случайное число: "  + number1 + "  ");
+
+if (number1 < 0) number1 = -number1;
+
+if (ThirdDigit(number1) == -1)
+  Console.WriteLine("Где третья цифра(...");
+else
+  Console.WriteLine($"Третья цифра загаданного числа... Вуаля:  {ThirdDigit(number1)}!");
+int ThirdDigit(int number)
+{
+  int result = -1;
+  if (number >= 100)
+  {
+    while (number > 999)
+    {
+      number = number / 10;
+    }
+    result = number % 10;
+  }
+  return result;
+}
