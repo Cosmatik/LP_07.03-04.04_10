053616b72dde16d492e3179bc15ab7596d95919f
Cosmatik <biznespro2021@yandex.ru>
2023-03-15 18:37:53 +0300

    Добавили решение к задаче 14

---
+++ Task14/Program.cs
@@ -0,0 +1,27 @@
+﻿// 14. 
+// Напишите программу, которая 
+// 1.принимает на вход число и 
+// 2. проверяет, кратно ли оно одновременно 7 и 23.
+
+
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+
+Console.WriteLine("Введите первое число на кратность:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число на кратность:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool result = MultTwoNumbers(number, number1, number2);
+Console.WriteLine(result ? "Да, все верно!" : "Нет!");
+
+bool MultTwoNumbers(int num, int num1, int num2)
+{
+  return num % num1 == 0 && num % num2 == 0;
+}
