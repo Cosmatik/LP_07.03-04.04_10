49db4f8697f89afc23d99a218f2e5ea7e2efd4e3
Cosmatik <biznespro2021@yandex.ru>
2023-04-05 12:54:05 +0300

    Добавили решение к задаче 67

---
+++ Task67/Program.cs
@@ -0,0 +1,23 @@
+﻿
+// Задача 67.
+// Напишите программу, которая будет принимать на вход число 
+// и возвращать сумму его цифр.
+
+// 453 -> 12
+//  45 -> 9
+
+
+
+Console.WriteLine("Вводим первое число:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+int SumDigits(int num)
+{
+    if (num > 0) return 
+    SumDigits(num / 10) + num % 10;
+    else return 0;
+}
+
+ Console.WriteLine($"Сумма цифр в числе: {SumDigits(n)}");
+ Console.WriteLine();
